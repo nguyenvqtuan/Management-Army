d79f92f20d8b2a979d5713a5837b9f990845723d
NVQ Tuan <nvquoctuan@gmail.com>
2022-02-22 15:01:17 +0700

    First commit

---
+++ src/main/java/com/ld575/quanlycsm/QuanLyCsmApplication.java
@@ -0,0 +1,13 @@
+package com.ld575.quanlycsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyCsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyCsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ld575/quanlycsm/QuanLyCsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld575.quanlycsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyCsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
