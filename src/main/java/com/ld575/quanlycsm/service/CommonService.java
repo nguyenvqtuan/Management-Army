84ea4e9b62efd379105cffc23008124779aee734
NVQ Tuan <nvquoctuan@gmail.com>
2022-02-28 23:02:36 +0700

    Update common service

--- src/main/java/com/ld575/quanlycsm/service/CommonService.java
+++ src/main/java/com/ld575/quanlycsm/service/CommonService.java
@@ -31,9 +31,13 @@ public class CommonService {
 		String [] arr = str.split("\\s");
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < arr.length; ++i) {
+			if (isEmpty(arr[i])) {
+				continue;
+			}
 			if (sb.length() != 0) {
 				sb.append(" ");
 			}
+			System.out.println(arr[i]);
 			sb.append(arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1));
 		}
 		return sb.toString().trim();
